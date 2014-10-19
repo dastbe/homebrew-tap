e071a12ce13c1f624ba8dccebf49079b5e5741c4
David Bell <dastbe@me.com>
2014-10-19 12:56:39 -0700

    cargo-nightly

---
+++ cargo-nightly.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class CargoNightly < Formula
+   homepage 'http://crates.io'
+   head 'https://static.rust-lang.org/cargo-dist/cargo-nightly-x86_64-apple-darwin.tar.gz'
+
+   depends_on 'rust-nightly'
+
+   def install
+      args = ['--prefix='prefix]
+      system './install.sh', *args
+   end
+
+   test do
+      system bin'/rustc'
+      system bin'/rustdoc', '-h'
+   end
+end
