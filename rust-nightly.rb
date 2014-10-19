82989d3e02da5d2b7f58207e68ddba54471c380c
David Bell <dastbe@me.com>
2014-10-19 12:57:02 -0700

    update rust-nightly

--- rust-nightly.rb
+++ rust-nightly.rb
@@ -1,19 +1,18 @@
 require 'formula'
-require 'date'
 
 class RustNightly < Formula
    homepage 'http://www.rust-lang.org'
    head 'http://static.rust-lang.org/dist/rust-nightly-x86_64-apple-darwin.tar.gz'
 
-   conflicts_with 'rust', :because => 'same'
+   conflicts_with 'rust', :because => 'it is the same the binary'
 
    def install
-      args = ["--prefix=#{prefix}"]
-      system "./install.sh", *args
+      args = ['--prefix='prefix]
+      system './install.sh', *args
    end
 
    test do
-      system "#{bin}/rustc"
-      system "#{bin}/rustdoc", "-h"
+      system bin'/rustc'
+      system bin'/rustdoc', '-h'
    end
 end
